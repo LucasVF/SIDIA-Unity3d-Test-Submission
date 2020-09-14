00d57330e876c8d2e7073a1f3ee3e35cf93df98c
LucasVF <lucas_v.f@hotmail.com>
2020-09-14 02:01:13 -0300

    Shaders Included

---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script to control Obstacles Spawned
+public class ObstacleController : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If object has passed Player it should be destroyed
+        //Some leeway is given so that object do not disapear on screen
+        if (transform.position.z < player.transform.position.z- 1.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
